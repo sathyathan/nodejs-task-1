b36430654f4a00b91103b06f9ea7cc80f9981cd3
sathaythan <sathyathangavel7@gmail.com>
2024-05-28 22:39:05 +0530

    updated

--- index.js
+++ index.js
@@ -12,12 +12,6 @@ const PORT = 4000;
 //Middleware
 app.use(express.json())
 
-//routes
-app.get('/',(req,res)=>{
-   // res.status(200).json({message:"Hi all welcome to our first node app"})
-   res.status(200).send(`<span style="background-color:Aqua;color:black;font-size:100px">Welcome to our first app in nodejs</span>`)
-})
-
 
 app.get('/create-read',(req,res)=>{
   let today = format(new Date(),'dd-mm-yyyy-HH-mm-ss')
